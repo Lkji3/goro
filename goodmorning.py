6dc33f9064a317b150b437c576993f76491a70cb
Lkji3 <152702041+Lkji3@users.noreply.github.com>
2025-04-10 12:57:54 +0300

    Update goodmorning.py

--- goodmorning.py
+++ goodmorning.py
@@ -14,7 +14,7 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',
     'RqUID': '5554e856-6c64-4054-b879-decca792da15',  # Уникальный ID запроса
-    'Authorization': 'Basic MjM1MGJiYmYtNzI1ZS00ZGQzLWFkNWUtMjk2ZDdiMjI2MGQ0OjA5MTcyMmIyLThjOTItNDQwYS05Mzc2LTBiMGJmOWYxOWY4Ng=='  # Замените на свой ключ
+    'Authorization': 'Basic MjM1MGJiYmYtNzI1ZS00ZGQzLWFkNWUtMjk2ZDdiMjI2MGQ0OmUyN2RiMDkzLWM0Y2YtNGE0MS1hNjE2LThmMTVmNWNmOGEwMA=='  # Замените на свой ключ
 }
 
 # Отправка запроса на получение токена
@@ -153,15 +153,14 @@ else:
 
 
 # Тема + ответ
-post = f"Доброе утро \n\n{answer}"
+post = f"Доброй ночи \n\n{answer}"
 
 print("\n--- Генерация поста ---")
 print(post)
 
 # --- Генерация изображения ---
 
-class Text2ImageAPI:
-
+class FusionBrainAPI:
     def __init__(self, url, api_key, secret_key):
         self.URL = url
         self.AUTH_HEADERS = {
@@ -169,65 +168,48 @@ class Text2ImageAPI:
             'X-Secret': f'Secret {secret_key}',
         }
 
-    def get_model(self):
-        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+    def get_pipeline(self):
+        response = requests.get(self.URL + 'key/api/v1/pipelines', headers=self.AUTH_HEADERS)
         data = response.json()
         return data[0]['id']
 
-    def generate(self, prompt, model, images=1, width=1024, height=1024):
+    def generate(self, prompt, pipeline, images=1, width=1024, height=1024):
         params = {
             "type": "GENERATE",
             "numImages": images,
             "width": width,
             "height": height,
             "generateParams": {
-                "query": f"{prompt}"
+                "query": f'{prompt}'
             }
         }
 
         data = {
-            'model_id': (None, model),
+            'pipeline_id': (None, pipeline),
             'params': (None, json.dumps(params), 'application/json')
         }
-        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        response = requests.post(self.URL + 'key/api/v1/pipeline/run', headers=self.AUTH_HEADERS, files=data)
         data = response.json()
         return data['uuid']
 
-    def check_generation(self, request_id, attempts=40, delay=10):
+    def check_generation(self, request_id, attempts=10, delay=10):
         while attempts > 0:
-            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            response = requests.get(self.URL + 'key/api/v1/pipeline/status/' + request_id, headers=self.AUTH_HEADERS)
             data = response.json()
             if data['status'] == 'DONE':
-                print(f"Изображение сгенерировано успешно: {data['images']}")
-                return data['images']
+                return data['result']['files']
 
             attempts -= 1
-            print(f"Задача в статусе {data['status']}, попытка {40 - attempts}...")
             time.sleep(delay)
-        print("Не удалось получить изображение после завершения генерации.")
         return None
 
-    def download_image(self, image_base64, file_path):
-        # Декодируем Base64 в байты
-        image_data = base64.b64decode(image_base64)
-        
-        # Записываем байты в файл
-        with open(file_path, 'wb') as file:
+    def save_image(self, base64_image, filename):
+        image_data = base64.b64decode(base64_image)
+        with open(filename, 'wb') as file:
             file.write(image_data)
-            print(f"Изображение сохранено по пути: {file_path}")
-        return file_path
-
-
-# Пример использования API Kandinsky
-api_url = 'https://api-key.fusionbrain.ai/'
-api_key = 'B003A085CA0287E8EAE86ECC00B259FC'
-secret_key = '67505534A526F5635FAFEAABED29C9F4'
-
-api = Text2ImageAPI(api_url, api_key, secret_key)
-
+        return filename
 
 def generate_image_prompt():
-    # Список вариантов для генерации изображений
     prompts = [
         "Живописное утро в горах с солнцем",
         "Тёплый солнечный свет над зелёным полем",
@@ -241,35 +223,39 @@ def generate_image_prompt():
     return random.choice(prompts)
 
 try:
-    # Выбор модели
-    model_id = api.get_model()
-    print(f"Модель выбрана: {model_id}")
+    # Инициализация API
+    api = FusionBrainAPI('https://api-key.fusionbrain.ai/', 
+                        'B003A085CA0287E8EAE86ECC00B259FC', 
+                        '67505534A526F5635FAFEAABED29C9F4')
+    
+    # Получение pipeline
+    pipeline_id = api.get_pipeline()
+    print(f"Pipeline ID: {pipeline_id}")
 
     # Генерация изображения с динамическим запросом
     prompt = generate_image_prompt()
     print(f"Сгенерированный запрос для изображения: {prompt}")
-    uuid = api.generate(prompt, model_id)
+    uuid = api.generate(prompt, pipeline_id)
     print(f"Запрос на генерацию изображения отправлен, UUID: {uuid}")
 
     # Проверка статуса и получение изображения
     images_base64 = api.check_generation(uuid)
 
-    bot_token = '7825529193:AAH06S3J0b5NhvxqXI8UK7vte-HTddsC7vc'  # Токен вашего Telegram бота
-    channel_id = '@hyppolove'  # ID вашего канала (с @ на начале)
+    bot_token = '7825529193:AAEIIA_IrpWM4bb1EGqAWrBxJABlZKko7sE'
+    channel_id = -1002419012006
 
     # Форматируем текст сообщения
-    caption = post  # Убедитесь, что переменная `post` заранее заполнена
-    caption = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', caption)  # Заменяем **текст** на <b>текст</b>
+    caption = post
+    caption = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', caption)
 
-    # Ограничиваем длину текста до 1024 символов
     max_caption_length = 1024
     if len(caption) > max_caption_length:
         caption = caption[:max_caption_length]
 
     if images_base64:
-        print(f"Изображение получено: {images_base64[0]}")  # Вывод Base64 изображения
-        # Скачиваем изображение
-        image_path = api.download_image(images_base64[0], 'generated_image.png')
+        print("Изображение успешно сгенерировано")
+        # Сохраняем изображение
+        image_path = api.save_image(images_base64[0], 'generated_image.png')
 
         # Отправка изображения в Telegram
         with open(image_path, 'rb') as image_file:
